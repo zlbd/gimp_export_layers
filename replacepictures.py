2f7c645f2a499e4a281ac45719d65bc9d57de16a
zlbd <791597507@qq.com>
2018-10-17 04:09:38 +0800

    add script for replace pictures

--- replacepictures.py
+++ replacepictures.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
-# A template for gimp python-fu script
+# A replace_pictures for gimp python-fu script
 # Author: zlbd
 
 from gimpfu import *
 import os, sys, platform
-
-################################################################################
-# lib function
-################################################################################
-#pdb.file_psd_save(img, None, name, name, 1, 1)
 
 ################################################################################
 # define function
@@ -54,33 +49,66 @@ def get_imgpath(basepath, midFolder = ''):
             os.mkdir(imgpath)
     return imgpath;
 
-def walk_groupLayer(layerInput, func, param):
+def walk_groupLayer(layerInput, cb, params_outside):
     layers = layerInput.layers
     nCount = len(layers)
     for i in range(0, nCount):
         layer = layers[i]
         if(layer.__class__ == gimp.GroupLayer):
-            if(walk_groupLayer(layer, func, param)):
+            if(walk_groupLayer(layer, cb, params_outside)):
                 return True
         elif(layer.__class__ == gimp.Layer):
-            if(func(layer, param, layers, i)):
+            params_walk = [layers, i, layerInput]
+            if(cb(layer, params_outside, params_walk)):
                 return True
         else:
             print("[Error] layer class is wrong: "+layer.name)
     return False
 
-def main_template(sDir, sFmt, img):
+def set_layer_rect(layer, rect):
+    x, y, w, h = rect
+    layer.set_offsets(x, y)
+    layer.scale(w, h)
+
+def replace_picture(layer, params_outside, params_walk):
+    sDir, sFmt, img = params_outside
+    layers, index, layerParent = params_walk
+    fName = sDir + layer.name + "." + sFmt
+    if not os.path.exists(fName):
+        return False
+    # load image as new layer
+    newlayer = pdb.gimp_file_load_layer(img, fName)
+    if(layerParent == img):
+        layerParent = 0
+    pdb.gimp_image_set_active_layer(img, layer)
+    pdb.gimp_image_insert_layer(img, newlayer, None, -1)
+    # move new layer
+    rect = layer.offsets[0], layer.offsets[1], layer.width, layer.height
+    set_layer_rect(newlayer, rect)
+    # merge down
+    layer.visible = True
+    newlayer.visible = True
+    layer = pdb.gimp_image_merge_down(img, newlayer, 0)
+    return False
+
+
+def main_replace_pictures(sDir, sFmt, img):
     #### protect code
     if( img == None ):
         gimp.message("[Error] Image None    \nPlease open a psd file...    ")
         return
     if( sDir == "" or sDir == None ):
         sDir = os.path.dirname(img.filename)
+    else:
+        sDir += get_slash()
     #### get base path
     #img = gimp.image_list()[0]
     layer = img.active_layer
     rect = layer.offsets[0], layer.offsets[1], layer.width, layer.height
-    gimp.message("Template run OK!" + "\n" 
+    #gimp.message(sDir)
+    params = [sDir, sFmt, img]
+    walk_groupLayer(img, replace_picture, params)
+    gimp.message("replace_pictures run OK!" + "\n" 
             + "The rectangle of layer selected:" + "\n\n" 
             + str(rect))
     return
@@ -90,13 +118,13 @@ def main_template(sDir, sFmt, img):
 ################################################################################
 init_log()
 register(
-    "python_fu_template",
-    "A simple Python-Fu 'template' plug-in.",
+    "python_fu_replace_pictures",
+    "A simple Python-Fu 'replace_pictures' plug-in.",
     "When run this plug-in, show 'Test OK' message.",
     "zlbd",
     "zlbd 2018.",
     "2018",
-    "<Toolbox>/Layer/_Template(Py)",
+    "<Toolbox>/Layer/_Replace Pictures (Py)",
     "RGB*, GRAY*",
     [
         (PF_DIRNAME, "string", "Directory",     None),
@@ -104,6 +132,6 @@ register(
         (PF_IMAGE,   "image",  "Input Sselect", None),
     ],
     [],
-    main_template)
+    main_replace_pictures)
  
 main()
